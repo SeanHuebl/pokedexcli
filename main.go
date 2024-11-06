e46a98d0c07448b3ca409d545d3c0d04be0adf8d
Sean Huebl <sean.huebl@gmail.com>
2024-11-06 05:35:26 -0700

    Added cliCommand struct with some helper functions. The program now displays the help docs when 'help' is input into the cli, and exits the program when 'exit' is input.

--- main.go
+++ main.go
@@ -6,7 +6,42 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex:")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exits the program")
+	fmt.Println("")
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
 func main() {
+
+	/* commands := map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+
+		"exit": {
+			name:        "exit",
+			description: "Exits the program",
+			callback:    commandExit,
+		},
+	} */
+
 	ch := make(chan string)
 	go func() {
 		defer close(ch)
@@ -19,7 +54,13 @@ func main() {
 	for {
 		fmt.Print("pokedex > ")
 		value := <-ch
-		fmt.Println(value)
-	}
+		fmt.Println("")
+		if value == "help" {
+			commandHelp()
+		}
+		if value == "exit" {
+			commandExit()
+		}
 
+	}
 }
