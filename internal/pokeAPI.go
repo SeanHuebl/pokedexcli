54915291c8541e5f6c24d2df038c14c8bdafdacc
Sean Huebl <sean.huebl@gmail.com>
2024-11-12 06:03:08 -0700

    added a Catch() that attempts to catch a pokemon. It calls the API and bases the catch rate off of the base experience. If the pokemon is caught it adds it to the global var Pokedex which is a map[string ]pokemon. Still need to implement the functionality into the main function.

--- internal/pokeAPI.go
+++ internal/pokeAPI.go
@@ -5,8 +5,62 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 )
+
+type Pokemon struct {
+	Abilities              []Abilities `json:"abilities"`
+	BaseExperience         int         `json:"base_experience"`
+	Height                 int         `json:"height"`
+	HeldItems              []any       `json:"held_items"`
+	ID                     int         `json:"id"`
+	LocationAreaEncounters string      `json:"location_area_encounters"`
+	Moves                  []Moves     `json:"moves"`
+	Name                   string      `json:"name"`
+	Species                Species     `json:"species"`
+	Stats                  []Stats     `json:"stats"`
+	Types                  []Types     `json:"types"`
+	Weight                 int         `json:"weight"`
+	CatchChance            float32
+}
+type Ability struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Abilities struct {
+	Ability  Ability `json:"ability"`
+	IsHidden bool    `json:"is_hidden"`
+	Slot     int     `json:"slot"`
+}
+type Move struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Moves struct {
+	Move Move `json:"move"`
+}
+type Species struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Stat struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Stats struct {
+	BaseStat int  `json:"base_stat"`
+	Effort   int  `json:"effort"`
+	Stat     Stat `json:"stat"`
+}
+type Type struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Types struct {
+	Slot int  `json:"slot"`
+	Type Type `json:"type"`
+}
 
 type Location struct {
 	Count    int     `json:"count"`
@@ -35,6 +89,8 @@ var Conf = Config{
 	Next:     "https://pokeapi.co/api/v2/location-area/",
 	Previous: nil,
 }
+
+var Pokedex map[string]Pokemon
 
 func CommandMap(cF *Config, c *Cache) error {
 	var location Location
@@ -158,5 +214,55 @@ func Explore(c *Cache, areaName string) error {
 	for _, encounter := range encounters.PokemonEncounters {
 		fmt.Printf("- %s\n", encounter.Pokemon.Name)
 	}
+	return nil
+}
+func Catch(c *Cache, pokemonName string) error {
+	pokemonUrl := "https://pokeapi.co/api/v2/" + pokemonName
+	fmt.Printf("Throwing a ball at %s...\n", pokemonName)
+	var pokemon Pokemon
+	v, ok := c.entry[pokemonName]
+	if !ok {
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", pokemonUrl, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer res.Body.Close()
+		data, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.Add(pokemonUrl, data)
+		if err = json.Unmarshal(data, &pokemon); err != nil {
+			return err
+		}
+	} else {
+		if err := json.Unmarshal(v.val, &pokemon); err != nil {
+			return err
+		}
+	}
+	if pokemon.BaseExperience < 70 {
+		pokemon.CatchChance = .85
+	} else if pokemon.BaseExperience < 160 {
+		pokemon.CatchChance = .50
+	} else if pokemon.BaseExperience < 250 {
+		pokemon.CatchChance = .25
+	} else {
+		pokemon.CatchChance = .10
+	}
+
+	catchRoll := rand.Float32()
+	if catchRoll < pokemon.CatchChance {
+		fmt.Printf("%s was caught!\n", pokemonName)
+		Pokedex[pokemonName] = pokemon
+		return nil
+	}
+	fmt.Printf("%s escaped!\n", pokemonName)
 	return nil
 }
